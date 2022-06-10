2b7d39f810e1f8e426eb93ca680c048e81668cac
Wojciech18046 <wwx9968@student.wsb.warszawa.pl>
2022-06-10 16:49:35 +0200

    Added I Publication interface. Added LibraryItem class.

---
+++ LibraryOP/LibraryOP/LibraryItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LibraryOP
+{
+    public class LibraryItem
+    {
+        public int Id { get; }
+        public int BarCode { get; }
+        public string Name { get; }
+        public bool IsRented { get; set; }
+        public int? RentedById { get; set; }
+        public LibraryItem(int barCode, string name)
+        {
+            //TODO: add logic to generate ID
+            //this.Id = GenerateId(); 
+            this.BarCode = barCode;
+            this.Name = name;
+            this.IsRented = false;
+            this.RentedById = null;
+        }
+
+        void Delete()
+        {
+            //TODO: add method body
+            throw new NotImplementedException();
+        }
+    }
+}

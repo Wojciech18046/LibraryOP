4829c361652d5cf8dd834e4c3c6754806bdb4379
Wojciech18046 <wwx9968@student.wsb.warszawa.pl>
2022-06-10 19:09:01 +0200

    Added User class

---
+++ LibraryOP/LibraryOP/User.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryOP
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public User(string name, string Email, string Address)
+        {
+            //TODO: Add Id generation
+            //this.Id = 
+            this.Name = name;
+            this.Email = Email;
+            this.Address = Address; 
+        }
+    }
+}

b8b2ae0e9a40c25706b99be2cd37e49d8aa65f19
Wojciech18046 <wwx9968@student.wsb.warszawa.pl>
2022-06-10 13:03:02 +0200

    Added new solution for the Library project. Added changes to gitignore file.

---
+++ LibraryOP/LibraryOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

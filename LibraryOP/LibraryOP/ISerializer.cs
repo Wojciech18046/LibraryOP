5ee3eb0cf2282dddfcca2ec3410fbe51b0ae2039
BartłomiejSwit <switbartlomiej@gmail.com>
2022-06-16 13:05:59 +0200

    Serializacja

---
+++ LibraryOP/LibraryOP/ISerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryOP
+{
+    internal interface ISerializer<T>
+    {
+        string Serialize(T obj);
+        T Deserialize(string content);
+    }
+}
---
+++ LibraryOP/LibraryOP/Serializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace LibraryOP
+{
+    internal class Serializer<T> : ISerializer<T>
+    {
+        public T Deserialize(string content)
+        {
+            return JsonSerializer.Deserialize<T>(content);
+        }
+        public string Serialize(T obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+    }
+}

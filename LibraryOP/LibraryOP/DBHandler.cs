3b766f0fc751e3954816e9e262403b9f308dc702
Wojciech18046 <wwx9968@student.wsb.warszawa.pl>
2022-06-16 17:46:21 +0200

    DBHandler draft

---
+++ LibraryOP/LibraryOP/DBHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryOP
+{
+    public static class DBHandler
+    {
+        private const string BookDBName = "Books.json";
+        private const string MovieDBName = "Movies.json";
+        private const string MagazineDBName = "Magazines.json";
+        private const string ScientificPaperDBName = "ScientificPapers.json";
+        private const string UserDBName = "Users.json";
+
+        public List<T> ReadDb<T>()
+            where T : LibraryItem
+        {
+            var list = new List<T>();
+
+            
+
+        }
+    }
+}

5d3d537019ce3d9560c9dcc2b3ac33bffd8c268c
Tymoteusz18037 <wwx8279@student.wsb.warszawa.pl>
2022-06-16 13:22:57 +0200

    Typo _in_line_12

--- LibraryOP/LibraryOP/ILibrary.cs
+++ LibraryOP/LibraryOP/ILibrary.cs
@@ -9,7 +9,7 @@ namespace LibraryOP
         public void AddItem(LibraryItem item);
         public void RemoveItem(int id);
         public void RentItem(int id, int userid);
-        public void ReturnItem(int it);
+        public void ReturnItem(int id);
         public void ListItems();
         public void AddUser(User user);
         public void RemoveUser(int id);
